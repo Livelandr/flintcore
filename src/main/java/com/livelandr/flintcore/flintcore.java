5d88c9a11b11b1d93e2725addc4e7542ec6db5fb
Livelandr <74109114+Livelandr@users.noreply.github.com>
2026-05-29 21:20:19 +0300

    license

--- src/main/java/com/livelandr/flintcore/flintcore.java
+++ src/main/java/com/livelandr/flintcore/flintcore.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2026 Livelandr
+ *
+ * This file is part of Flintcore.
+ *
+ * Flintcore is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Flintcore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.livelandr.flintcore;
 
 import com.livelandr.flintcore.core.network.PacketHandler;
